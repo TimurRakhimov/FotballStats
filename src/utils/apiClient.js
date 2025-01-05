b62c9d193b2e3ab95ed8566bf7914e85d475b05e
TimurRakhimov <63906749+TimurRakhimov@users.noreply.github.com>
2025-01-04 18:17:04 -0800

    Add API client file

---
+++ src/utils/apiClient.js
@@ -0,0 +1,12 @@
+// src/utils/apiClient.js
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.REACT_APP_API_KEY}`,
+    },
+});
+
+export default apiClient;
