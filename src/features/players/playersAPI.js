45d9cccdc532c486cf3f341ee17350d113af5281
TimurRakhimov <63906749+TimurRakhimov@users.noreply.github.com>
2025-01-07 12:05:35 -0800

    Fix cors issues

--- src/features/players/playersAPI.js
+++ src/features/players/playersAPI.js
@@ -1,7 +1,4 @@
 const cache = {}; // In-memory cache
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://api.football-data.org' : '/api';
-console.log('API Base URL:', process.env.REACT_APP_API_BASE_URL);
-console.log('API Key:', process.env.REACT_APP_API_KEY);
 
 export const fetchPlayers = async () => {
     const cacheKey = 'team-86'; // Unique key for this request
@@ -13,12 +10,15 @@ export const fetchPlayers = async () => {
     }
 
     try {
-        const response = await fetch(`${baseURL}/v3/teams/86`, {
-            method: 'GET',
-            headers: {
-                'X-Auth-Token': process.env.REACT_APP_API_KEY,
-            },
-        });
+        const response = await fetch(
+            `https://cors-anywhere.herokuapp.com/https://api.football-data.org/v3/teams/86`,
+            {
+              method: "GET",
+              headers: {
+                "X-Auth-Token": process.env.REACT_APP_API_KEY,
+              },
+            }
+          );
 
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
