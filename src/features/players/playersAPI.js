93e4f6a0c05c1eabb7b65e06b4e3e55aa6ed2b95
TimurRakhimov <63906749+TimurRakhimov@users.noreply.github.com>
2025-01-06 23:25:37 -0800

    Fix issues

--- src/features/players/playersAPI.js
+++ src/features/players/playersAPI.js
@@ -30,34 +30,3 @@ export const fetchPlayers = async () => {
         throw error;
     }
 };
-
-export const fetchPlayer = async (id) => {
-    const cacheKey = `player-${id}`; // Unique key for each player
-
-    // Check if data exists in cache
-    if (cache[cacheKey]) {
-        console.log(`Using cached data for player ${id}`);
-        return cache[cacheKey];
-    }
-
-    try {
-        const response = await fetch(`/api/persons/${id}`, {
-            method: 'GET',
-            headers: {
-                'X-Auth-Token': 'c625dda22c0341c6b7ad3874162e1d37',
-            },
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const jsonResponse = await response.json();
-        cache[cacheKey] = jsonResponse; // Store response in cache
-        console.log(`Fetched data for player ${id} from API`);
-        return jsonResponse;
-    } catch (error) {
-        console.error(`Error fetching data for player ${id}:`, error);
-        throw error;
-    }
-};
